4d770d08e4af576487a8c7dded4520dd666ef81f
Boburjon Murodov <boburjonmurodov06@gmail.com>
2025-03-09 14:08:09 +0500

    made parts scrollable

--- composeApp/src/commonMain/kotlin/com/boboor/speaking/ui/screens/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/boboor/speaking/ui/screens/main/MainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,7 +25,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -237,6 +240,7 @@ private fun MainScreenContent(
 
             Column(
                 modifier = Modifier.weight(1f)
+                    .verticalScroll(rememberScrollState())
                     .fillMaxWidth()
                     .padding(horizontal = 24.dp),
 
