d4125ebfe20fd8ff914be5d80c6aa5d61503dc0d
Boburjon Murodov <boburjonmurodov06@gmail.com>
2025-03-11 12:59:15 +0500

    fixed api service

--- composeApp/src/commonMain/kotlin/com/boboor/speaking/data/remote/ApiService.kt
+++ composeApp/src/commonMain/kotlin/com/boboor/speaking/data/remote/ApiService.kt
@@ -2,7 +2,6 @@ package com.boboor.speaking.data.remote
 
 import com.boboor.speaking.data.remote.models.CommonTopicResponse
 import com.boboor.speaking.data.remote.models.PartTwoResponse
-import com.boboor.speaking.utils.resultOf
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -28,19 +27,11 @@ class ApiService(private val httpClient: HttpClient) {
         return@withContext httpClient.get("$BASE_URL$SECTION_ONE").body();
     }
 
-    suspend fun getPartTwoQuestions(): Result<PartTwoResponse.Response> {
-        resultOf { httpClient.get("$BASE_URL$SECTION_TWO") }
-            .onSuccess { return Result.success(it.body()) }
-            .onFailure { return Result.failure(it) }
-
-        return Result.failure(Exception("Unknown error"))
+    suspend fun getPartTwoQuestions(): Result<PartTwoResponse.Response> = withContext(Dispatchers.IO) {
+        return@withContext httpClient.get("$BASE_URL$SECTION_TWO").body()
     }
 
-    suspend fun getPartThreeQuestions(): Result<CommonTopicResponse.Response> {
-        resultOf { httpClient.get("$BASE_URL$SECTION_THREE") }
-            .onSuccess { return Result.success(it.body()) }
-            .onFailure { return Result.failure(it) }
-
-        return Result.failure(Exception("Unknown error"))
+    suspend fun getPartThreeQuestions(): CommonTopicResponse.Response = withContext(Dispatchers.IO) {
+        return@withContext httpClient.get("$BASE_URL$SECTION_THREE").body()
     }
 }
