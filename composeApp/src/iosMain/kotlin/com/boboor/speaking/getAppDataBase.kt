7961eed3d53cd5f964cd1f36bf757f5a4646987c
Boburjon Murodov <boburjonmurodov06@gmail.com>
2025-04-23 10:46:03 +0500

    feat: Implement Platform-Specific Room Database Builders
    This commit introduces platform-specific implementations for creating a Room database builder, enabling the use of Room database on both iOS and Android.
    
    -   **iOS Implementation:**
        -   Created `getAppDataBase.kt` in `iosMain` to provide a Room database builder.
        -   Utilizes `Room.databaseBuilder` to construct the database.
        -   Defines the database file path within the application's document directory.
        -   Added `documentDirectory` to manage the file path in iOS.
    -   **Android Implementation:**
        -   Created `getDataBase.kt` in `androidMain` to provide a Room database builder.
        -   Uses `Room.databaseBuilder` with the application context.
        -  Added `SQLiteDatabase` to manage the database file on Android.
        -   Defines the database file path using `getDatabasePath`.
    - **Common database package**
        - Add empty `getAppDataBase.kt` under `appleMain` to avoid compile errors.

---
+++ composeApp/src/iosMain/kotlin/com/boboor/speaking/getAppDataBase.kt
@@ -0,0 +1,37 @@
+package com.boboor.speaking
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import com.boboor.speaking.data.local.room.AppDataBase
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSUserDomainMask
+
+
+/*
+    Created by Boburjon Murodov 21/04/25 at 18:20
+*/
+
+
+
+
+fun getDatabaseBuilder(): RoomDatabase.Builder<AppDataBase> {
+    val dbFilePath = documentDirectory() + "/questions.db"
+    return Room.databaseBuilder<AppDataBase>(
+        name = dbFilePath,
+    )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+
+    return requireNotNull(documentDirectory?.path)
+}
