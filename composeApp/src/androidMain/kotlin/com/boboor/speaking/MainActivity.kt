3b6989ed0c6ee8c4bb8341baa03085970e560b46
Boburjon Murodov <74082721+BoburjonMurodov@users.noreply.github.com>
2025-04-24 22:51:20 +0500

    added try catch while loading system library

--- composeApp/src/androidMain/kotlin/com/boboor/speaking/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/boboor/speaking/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.boboor.speaking.ui.theme.AppTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-        )
+        enableEdgeToEdge()
         setContent {
             App()
         }
@@ -25,3 +28,4 @@ class MainActivity : ComponentActivity() {
 fun AppAndroidPreview() {
     App()
 }
+
