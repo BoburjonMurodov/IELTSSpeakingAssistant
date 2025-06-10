d4e37576c8f8b95db185e35ca5053ee37c53b225
Boburjon Murodov <74082721+BoburjonMurodov@users.noreply.github.com>
2025-06-10 17:05:48 +0500

    Refactor: Migrate from Room to SQLDelight
    This commit migrates the local database from Room to SQLDelight.
    The `AppDataBase.kt` file for Room was deleted.
    New files for SQLDelight were added:
    - `AppDataBase.kt` (common)
    - `AppDataBase.android.kt` (Android specific)
    - `AppDataBase.ios.kt` (iOS specific)
    
    These changes establish the basic structure for using SQLDelight for local data persistence in the Android and iOS targets.

---
+++ composeApp/src/androidMain/kotlin/com/boboor/speaking/data/local/sql/AppDataBase.android.kt
@@ -0,0 +1,19 @@
+package com.boboor.speaking.data.local.sql
+
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.boboor.AppDatabase
+
+
+class AndroidDatabaseDiverFactory(
+    private val context: Context
+): DatabaseDriverFactory{
+    override fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(
+            AppDatabase.Schema,
+            context,
+            "questions.db"
+        )
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/boboor/speaking/data/local/sql/AppDataBase.ios.kt
@@ -0,0 +1,14 @@
+package com.boboor.speaking.data.local.sql
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import com.boboor.AppDatabase
+
+
+class AndroidDatabaseDiverFactory(): DatabaseDriverFactory{
+    override fun createDriver(): SqlDriver {
+        return NativeSqliteDriver(
+            AppDatabase.Schema, "questions.db"
+        )
+    }
+}
