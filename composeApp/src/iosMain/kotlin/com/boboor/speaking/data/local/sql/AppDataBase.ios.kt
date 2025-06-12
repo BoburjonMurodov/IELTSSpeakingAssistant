e8ae2e1172e53510c34a00a20f1a09d944f829ba
Boburjon Murodov <74082721+BoburjonMurodov@users.noreply.github.com>
2025-06-12 14:22:08 +0500

    Refactor: Comment out SQLDelight database driver factories
    This commit comments out the `AndroidDatabaseDiverFactory` classes in both the `iosMain` and `androidMain` source sets. These classes were responsible for creating platform-specific SQLDelight database drivers.
    
    Additionally, the app icon file `app-icon-1024.png` was deleted from the iOS project.

--- composeApp/src/iosMain/kotlin/com/boboor/speaking/data/local/sql/AppDataBase.ios.kt
+++ composeApp/src/iosMain/kotlin/com/boboor/speaking/data/local/sql/AppDataBase.ios.kt
@@ -1,14 +1,14 @@
 package com.boboor.speaking.data.local.sql
 
-import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import com.boboor.AppDatabase
-
-
-class AndroidDatabaseDiverFactory(): DatabaseDriverFactory{
-    override fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(
-            AppDatabase.Schema, "questions.db"
-        )
-    }
-}
+//import app.cash.sqldelight.db.SqlDriver
+//import app.cash.sqldelight.driver.native.NativeSqliteDriver
+//import com.boboor.AppDatabase
+//
+//
+//class AndroidDatabaseDiverFactory(): DatabaseDriverFactory{
+//    override fun createDriver(): SqlDriver {
+//        return NativeSqliteDriver(
+//            AppDatabase.Schema, "questions.db"
+//        )
+//    }
+//}
